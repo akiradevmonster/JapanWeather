a60c15ad01ed5a2791aa543d5c03ecf07906011b
Akira Yamamoto <devmonster2417@gmail.com>
2025-07-14 09:13:29 +0900

    Add main logic of date.ts

---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+function sameDay(d1: Date, d2: Date): boolean {
+	return d1.getFullYear() === d2.getFullYear() &&
+		d1.getMonth() === d2.getMonth() &&
+		d1.getDate() === d2.getDate()
+}
+
+export { sameDay }
