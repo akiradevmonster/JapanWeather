827680982301b58415597c02555421e49dc23b61
Akira Yamamoto <devmonster2417@gmail.com>
2025-07-24 09:03:58 +0900

    hotfix: change main logic of Footer.vue

---
+++ src/components/app/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-footer app>
+		<span class="caption">
+			<v-icon small>mdi-code-braces</v-icon>&nbsp;with
+			<v-icon color="error" small>mdi-heart</v-icon>&nbsp;by
+			<a href="https://github.com/OneWinged-Eagle" target="_blank">OneWinged-Eagle</a>.
+		</span>
+
+		<v-spacer />
+
+		<span class="caption">
+			<a href="https://github.com/OneWinged-Eagle/JapanWeather" target="_blank">
+				<v-icon>mdi-github-circle</v-icon>
+				<sup>
+					<v-icon x-small>mdi-open-in-new</v-icon>
+				</sup>
+			</a>
+		</span>
+	</v-footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Component from "vue-class-component"
+
+@Component({
+	name: "Footer"
+})
+export default class Footer extends Vue { }
+</script>
